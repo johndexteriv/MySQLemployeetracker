87a344dc6970a783de34d16eff2d040324150e4f
John Garner <jdextergarneriv@gmail.com>
2020-11-19 15:18:52 -0500

    created mysql connection and developed connection function

--- mybusiness.js
+++ mybusiness.js
@@ -1,2 +1,15 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "root",
+	database: "myBusinessDB",
+});
+
+connection.connect(function (err) {
+	if (err) throw err;
+	console.log("connected as id" + connection.threadId + "\n");
+});
